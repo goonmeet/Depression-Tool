48719bb5616d4966c367b13412f05eae0442c33a
Goonmeet <bajajkid@gmail.com>
2017-04-03 16:53:02 -0400

    Updated files and now has bootstrap

--- Hello.py
+++ Hello.py
@@ -1,36 +1,257 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask_googlemaps import GoogleMaps, Map, icons
+from flask_bootstrap import Bootstrap
 import tweepy, json
-import es_users
+import es_users, textblob
+from textblob import TextBlob
 app = Flask(__name__)
+Bootstrap(app)
 ACCESS_TOKEN = ""
 ACCESS_SECRET = ""
 CONSUMER_KEY = ""
 CONSUMER_SECRET = ""
 KEYWORDS = []
 
+@app.route('/form')
+def form():
+   return render_template('form.html')
+
+@app.route('/sample')
+def sample():
+    print "HI"
+    user_objects = es_users.getAllUserScreenNames()
+    markers = []
+    mark_json = []
+    for u_o in user_objects:
+    	for x in es_users.getStoredTweets(u_o["screen_name"]):
+    		x = json.dumps(x)
+    		x = json.loads(x)
+    		#print x 
+    		if x["_source"]["geo"] is not None:
+    			z = {}
+    			z["user_img"] = str(u_o["user_object"]["_source"]["profile_image_url"])
+    			#print z["user_img"]
+    			z["screen_name"] = str(u_o["screen_name"])
+    			z["lat"] = x["_source"]["geo"]["coordinates"][0]
+    			z["lng"] = x["_source"]["geo"]["coordinates"][1]
+    			markers.append(tuple(x["_source"]["geo"]["coordinates"]))
+    			mark_json.append(z)
+    			
+    		# if "coordinates" in x["_source"].keys():
+#     			print x["_source"]
+		#u_o["user_object"]["_source"]["status"]["coordinates"]
+    markers = list(set(markers))
+    #print markers
+    m_list = [{'lat': 37.4419, 'lng': -122.1419}]
+    #print user_obj
+    #print len(markers)
+    #print markers[0]
+    #print mark_json[0]
+    print len(mark_json)
+    temp = {v['screen_name']:v for v in mark_json}.values()
+    mark_json = temp
+    print len(mark_json)
+    return render_template('sample.html', result = user_objects, m_list = markers, mark_json = mark_json)
+
+
+
 @app.route('/')
 def student():
     result = es_users.getAllUserScreenNames()
-    return render_template('user_profiles.html', result = result)
+    es_users.getAllUserScreenNamesWithGeo()
+    #print result
+    # markers=[{'icon': icons.alpha.B, 'lat':  37.4419, 'lng':  -122.1419, 'infobox': "Hello I am < b style='color:green;'>B< / b >!"},
+    #         { 'icon': icons.dots.blue, 'lat': 37.4300, 'lng': -122.1400, 'infobox': "Hello I am < b style='color:blue;'>BLUE< / b >!"},
+    #         {'icon': icons.dots.yellow, 'lat': 37.4500, 'lng': -122.1350, 'infobox': ("Hello I am < b style='color:#ffcc00;'> YELLOW < / b >!" "< h2 >It is HTML title< / h2 >" "< img src=' //placehold.it/50' >" "< br >Images allowed!")}
+    #     ]
+    m_list = [{'lat': 37.4419, 'lng': -122.1419}]
+    markers = [{'lat': 37.4419, 'lng': -122.1419}, {'lat': 37.4500, 'lng': -122.1419}, {'lat': 36.4419, 'lng': -120.1419}]
+    return render_template('user_profiles.html', result = result, m_list = m_list)
+
+def getAvgSentiment(user):
+	all_tweets = es_users.getStoredTweets(user)
+	print len(all_tweets)
+	sum_senti = 0
+	for tweet in all_tweets:
+		blob = TextBlob(tweet["_source"]["text"])
+    	if blob.detect_language() is not "en":
+    		#print blob
+    		try:
+    			blob = blob.translate(to='en')
+    			tweet["_source"]["text"] = blob
+    		except textblob.exceptions.NotTranslated:
+    			pass
+    		#print blob
+    	for sentence in blob.sentences:
+    		sum_senti = sum_senti + sentence.sentiment.polarity
+    		print sum_senti
+	avg_sent = (sum_senti/len(all_tweets))
+	print avg_sent
+	return avg_sent
 
 @app.route('/<user>')
-def hello_name(user):
-    result = es_users.getUser(user)
-    tweets = es_users.getStoredTweets(user)
-    return render_template('user_profile.html', user = result, tweets = tweets)
+def user_profile(user):
+    print "I'm running..."
+    user_profile = es_users.getUser(user)
+    #if len(user_profile) > 0:
+    tweets = es_users.getStoredTweetsSize(user, 100)
+    print len(tweets)
+    eng_tweets = []
+    for tweet in tweets:
+    	blob = TextBlob(tweet["_source"]["text"])
+    	if blob.detect_language() is not "en":
+    		#print blob
+    		try:
+    			blob = blob.translate(to='en')
+    			tweet["_source"]["text"] = blob
+    		except textblob.exceptions.NotTranslated:
+    			pass
+    		#print blob
+    	eng_tweets.append(tweet)
+    eng_tweets = eng_tweets[:10]
+    avg_sent = getAvgSentiment(user)
+    print avg_sent
+    #print user
+    #print user_profile
+    #print user_profile[0]
+    return render_template('user_profile.html', user = user_profile[0], tweets = eng_tweets, avg_sent = avg_sent)
+    #return None
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
    if request.method == 'POST':
       result = request.form
-      return render_template("result.html", result = result)
+      result_link = result["SCREEN_NAME"]
+      print result_link
+      print url_for('user_profile', user=result_link)
+      #return hello_name(result_link)
+      return redirect(url_for('user_profile', user=result_link))
+#       return render_template("user_profile", result = result)
+
+
+
+@app.route('/fullmap')
+def fullmap():
+# 	fullmap = Map(
+#         identifier="catsmap",
+#         lat=37.4419,
+#         lng=-122.1419,
+#         markers=[
+#             {
+#                 'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+#                 'lat':  37.4419,
+#                 'lng':  -122.1419,
+#                 'infobox': "<img src='cat1.jpg' />"
+#             },
+#             {
+#                 'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+#                 'lat': 37.4300,
+#                 'lng': -122.1400,
+#                 'infobox': "<img src='cat2.jpg' />"
+#             },
+#             {
+#                 'icon': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+#                 'lat': 37.4500,
+#                 'lng': -122.1350,
+#                 'infobox': "<img src='cat3.jpg' />"
+#             }
+#         ]
+# 	)
+    fullmap = Map(
+        identifier="fullmap",
+        varname="fullmap",
+        style=(
+            "height:100%;"
+            "width:100%;"
+            "top:0;"
+            "left:0;"
+            "position:absolute;"
+            "z-index:200;"
+        ),
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+            {
+                'icon': '//maps.google.com/mapfiles/ms/icons/green-dot.png',
+                'lat': 37.4419,
+                'lng': -122.1419,
+                'infobox': "Hello I am <b style='color:green;'>GREEN</b>!"
+            },
+            {
+                'icon': '//maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                'lat': 37.4300,
+                'lng': -122.1400,
+                'infobox': "Hello I am <b style='color:blue;'>BLUE</b>!"
+            },
+            {
+                'icon': icons.dots.yellow,
+                'title': 'Click Here',
+                'lat': 37.4500,
+                'lng': -122.1350,
+                'infobox': (
+                    "Hello I am <b style='color:#ffcc00;'>YELLOW</b>!"
+                    "<h2>It is HTML title</h2>"
+                    "<img src='//placehold.it/50'>"
+                    "<br>Images allowed!"
+                )
+            }
+        ],
+        # maptype = "TERRAIN",
+        # zoom="5"
+    )
+
+    return render_template('example_fullmap.html', fullmap=fullmap)
+
+#     fullmap = Map(
+#         identifier="fullmap",
+#         varname="fullmap",
+#         style=(
+#             "height:100%;"
+#             "width:100%;"
+#             "top:0;"
+#             "left:0;"
+#             "position:absolute;"
+#             "z-index:200;"
+#         ),
+#         lat=37.4419,
+#         lng=-122.1419,
+#         markers=[
+#             {
+#                 'icon': '//maps.google.com/mapfiles/ms/icons/green-dot.png',
+#                 'lat': 37.4419,
+#                 'lng': -122.1419,
+#                 'infobox': "Hello I am <b style='color:green;'>GREEN</b>!"
+#             },
+#             {
+#                 'icon': '//maps.google.com/mapfiles/ms/icons/blue-dot.png',
+#                 'lat': 37.4300,
+#                 'lng': -122.1400,
+#                 'infobox': "Hello I am <b style='color:blue;'>BLUE</b>!"
+#             },
+#             {
+#                 'icon': icons.dots.yellow,
+#                 'title': 'Click Here',
+#                 'lat': 37.4500,
+#                 'lng': -122.1350,
+#                 'infobox': (
+#                     "Hello I am <b style='color:#ffcc00;'>YELLOW</b>!"
+#                     "<h2>It is HTML title</h2>"
+#                     "<img src='//placehold.it/50'>"
+#                     "<br>Images allowed!"
+#                 )
+#             }
+#         ],
+#         # maptype = "TERRAIN",
+#         # zoom="5"
+#     )
+
+#     return render_template('example_fullmap.html', fullmap=fullmap)
+
 
 if __name__ == '__main__':
-   app.run(debug = True)
-
-
-
-
+    GoogleMaps(app, key = "AIzaSyDYRyuCbFOh_8ivTTnm1YjsXv4kKYWoPYc")
+    app.run(debug = False, use_reloader = False)
+    #app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
 
  # import tweepy
  # import json
--- es_users.py
+++ es_users.py
@@ -1,34 +1,219 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
 from elasticsearch_dsl.connections import connections
-es = Elasticsearch(['localhost:9200'])
+import json
+es = Elasticsearch(['localhost:9201'])
+    		#print user["_source"]["status"]["geo"]["coordinates"]
+
+def parse_response(list_of_objs, type):
+	all_obj = []
+	if type == "user":
+		for user in list_of_objs:
+			z = {}
+    		z["screen_name"] = user["_source"]["screen_name"]
+    		z["tweet_count"] = getStoredTweetCount(user["_source"]["screen_name"])
+    		z["user"] = user["_source"]
+    		z["coordinates"] = user["_source"]["status"]["coordinates"]["coordinates"]
+    		z["profile_image_url"] = user["_source"]["profile_image_url"]
+    		z["lat"] = tweet_json["_source"]["status"]["geo"]["coordinates"][0]
+    		z["lng"] = tweet_json["_source"]["status"]["geo"]["coordinates"][1]
+    		if "place" in user["_source"]["status"]:
+				z["place"] = user["_source"]["status"]["place"]
+				location = True
+    		all_obj.append(z)
+	if type == "tweet":
+		tweet_obj = {}
+		for tweet_json in list_of_objs:
+			location = False
+			if "place" in tweet_json["_source"]:
+				tweet_obj["place"] = tweet_json["_source"]["place"]
+				location = True
+			if "coordinates" in tweet_json:
+				if "coordinates" in tweet_json["coordinates"]:
+					tweet_obj["coordinates"] = tweet_json["coordinates"]["coordinates"]
+					location = True
+			if location:
+				tweet_json = json.loads(json.dumps(tweet_json))
+				tweet_obj["es_id"] = tweet_json["_id"]
+				tweet_obj["text"] = tweet_json["_source"]["text"]
+				tweet_obj["created_at"] = tweet_json["_source"]["created_at"]
+				tweet_obj["favorite_count"] = tweet_json["_source"]["favorite_count"]
+				tweet_obj["retweet_count"] = tweet_json["_source"]["retweet_count"]
+				tweet_obj["twitter_id_str"] = tweet_json["_source"]["id_str"]
+				tweet_obj["lang"] = tweet_json["_source"]["lang"]
+				tweet_obj["screen_name"] = str(tweet_json["_source"]["user"]["screen_name"])
+				tweet_obj["profile_image_url"] = tweet_json["_source"]["user"]["profile_image_url"]
+    			tweet_obj["lat"] = tweet_json["_source"]["geo"]["coordinates"][0]
+    			tweet_obj["lng"] = tweet_json["_source"]["geo"]["coordinates"][1]
+    			all_obj.append(tweet_obj)
+				#print tweet_obj["text"]
+
+	return all_obj
+
+
+
+def getAllTweetsWithCoordinates():
+    res = es.search(size = 1000, scroll = '3m', index="depressed_tweets", doc_type="tweepyTweet", body={ "query": {"exists": {"field": "coordinates.coordinates"}}, "aggs": {"group_by_tweets": {"terms": {"field": "user.screen_name.keyword"}}}}, request_timeout=60)
+    s_id = res['_scroll_id']
+    scroll_size = res['hits']['total']
+    all_tweets = res['hits']['hits']
+    #print "Total hits " + str(len(res['hits']['hits']))
+    #print res['hits']['total']
+    while (scroll_size > 0):
+        res = es.scroll(scroll_id = s_id, scroll = '3m' , request_timeout=160)
+        s_id = res['_scroll_id']
+        scroll_size = len(res['hits']['hits'])
+        tweets = res['hits']['hits']
+        all_tweets = all_tweets + tweets
+        #print "scroll_size: " + str(scroll_size)
+    print len(all_tweets)
+    return all_tweets
+
+def getAllUsersWithCoordinates():
+    user_obj = []
+    users_withTweets = []
+    usersTweetCounts = []
+    res_users = es.search(size = 1000, scroll = '3m', index="user_profiles", doc_type="Self_Reported_Profiles_40k", body={"query": {"exists": {"field": "status.geo.coordinates"}}, "size" : 100 }, request_timeout=60)
+    s_id = res_users['_scroll_id']
+    scroll_size = res_users['hits']['total']
+    all_tweets = res_users['hits']['hits']
+    while (scroll_size > 0):
+        res_users = es.scroll(scroll_id = s_id, scroll = '3m' , request_timeout=160)
+        s_id = res_users['_scroll_id']
+        scroll_size = len(res_users['hits']['hits'])
+        tweets = res_users['hits']['hits']
+        all_tweets = all_tweets + tweets
+        #print "scroll_size: " + str(scroll_size)
+        #print len(all_tweets)
+    print len(all_tweets)
+    return all_tweets
+
+def getAllCoordinates():
+	a = parse_response(getAllUsersWithCoordinates(), "user")
+	b = parse_response(getAllTweetsWithCoordinates(), "tweet")
+	c = a + b
+	return c
 
 def getAllUserScreenNames():
-    res = es.search(index="user_profiles", doc_type="user_profile", body={"query": {"match_all": {}},"size" : 1000})
-    profiles = res['hits']['hits']
-    return profiles
+    user_obj = []
+    users_withTweets = []
+    usersTweetCounts = []
+    res = es.search(index="depressed_tweets", doc_type="tweepyTweet", body={ "query": {"exists": {"field": "coordinates.coordinates"}}, "aggs": {"group_by_tweets": {"terms": {"field": "user.screen_name.keyword"}}}}, request_timeout=60)
+    res_users = es.search(index="user_profiles", doc_type="Self_Reported_Profiles_40k", body={"query": {"exists": {"field": "status.geo.coordinates"}}, "size" : 100 }, request_timeout=60)
+    res_users = res_users["hits"]["hits"]
+    for user in res_users:
+    	#print user["_source"]["status"]["geo"]["coordinates"]
+    	z = {}
+    	z["screen_name"] = user["_source"]["screen_name"]
+    	z["tweet_count"] = getStoredTweetCount(user["_source"]["screen_name"])
+    	z["user_object"] = user
+    	user_obj.append(z)
+    res = res['aggregations']['group_by_tweets']['buckets']
+    #print res[0]
+    profiles = res
+    #print len(profiles)
+
+    #print len(profiles)
+    for buckets in profiles:
+    	#print x
+    	#x = json.dumps(json.loads(x))
+    	profile = getUser(buckets['key'])
+    	#print y
+    	users_withTweets.append(profile[0])
+        #print getStoredTweetCount(x["key"])
+    	z = {}
+    	z["screen_name"] = buckets["key"]
+    	z["tweet_count"] = getStoredTweetCount(buckets["key"])
+    	z["user_object"] = profile[0]
+        #print profile[0]
+        #print z
+        user_obj.append(z)
+        usersTweetCounts.append(getStoredTweetCount(buckets["key"]))
+        # if getStoredTweetCount(x["key"]) > 25:
+#         	x = getUser(x)
+#         	users_withTweets.append(x)
+#         	print x
+#         	usersTweetCounts.append(getStoredTweetCount(x["_id"]))
+#             #print getStoredTweetCount(x["_id"])
+    user_obj.sort(key=lambda x: x['tweet_count'], reverse=True)
+    #print user_obj
+    #users_withTweets.sort(key=lambda x: getStoredTweetCount(x['_id']), reverse=True)
+    #print y
+    #return users_withTweets, usersTweetCounts
+    return user_obj
+
 
 
 def getUser(screen_name):
-    res = es.search(index="user_profiles", doc_type="user_profile", body={"query": { "match": {"_id" : screen_name}}})
+    res = es.search(index="user_profiles", doc_type="Self_Reported_Profiles_40k", body={"query": { "match": {"_id" : screen_name}}}, request_timeout=60)
     #print res
     user = res['hits']["hits"]
     return user
 
+# def getStoredTweets(screen_name, size):
+#     res = es.search(index="depressed_tweets", doc_type="tweepyTweet", body={"query": { "match": {"user.screen_name": screen_name}}, "size" : size}, request_timeout=60)
+#     #print res
+#     tweets = res['hits']["hits"]
+#     for x in tweets:
+#     	x = json.dumps(x)
+#     	x = json.loads(x)
+#     	#print x["_source"]["coordinates"]
+#     return tweets
+
 def getStoredTweets(screen_name):
-    res = es.search(index="depressed_tweets", doc_type="tweepyTweet", body={"query": { "match": {"user.screen_name": screen_name}}})
+    res = es.search(size = 1000, scroll = '3m', index="depressed_tweets", doc_type="tweepyTweet", body={"query": { "match": {"user.screen_name": screen_name}}}, request_timeout=160)
+    s_id = res['_scroll_id']
+    scroll_size = res['hits']['total']
     #print res
-    tweet_count = res['hits']["hits"]
-    return tweet_count
+    all_tweets = res['hits']['hits']
+    while (scroll_size > 0):
+        res = es.scroll(scroll_id = s_id, scroll = '3m' , request_timeout=160)
+        s_id = res['_scroll_id']
+        scroll_size = len(res['hits']['hits'])
+        tweets = res['hits']['hits']
+        all_tweets = all_tweets + tweets
+        print "scroll_size: " + str(scroll_size)
+    return all_tweets
+
+
+def getStoredTweetsSize(screen_name, size):
+    res = es.search(size = 10, index="depressed_tweets", doc_type="tweepyTweet", body={"query": { "match": {"user.screen_name": screen_name}}}, request_timeout=160)
+    all_tweets = res['hits']['hits']
+    return all_tweets
+
+
+
+def parse_tweet_obj(screen_name):
+	tweet_jsons = es_users.getStoredTweets(screen_name)
+	all_user_tweet_obj = []
+	tweet_obj = {}
+	for tweet_json in tweet_jsons:
+		tweet_json = json.loads(json.dumps(tweet_json))
+		tweet_obj["es_id"] = tweet_json["_id"]
+		tweet_obj["text"] = tweet_json["_source"]["text"]
+		tweet_obj["created_at"] = tweet_json["_source"]["created_at"]
+		tweet_obj["favorite_count"] = tweet_json["_source"]["favorite_count"]
+		tweet_obj["retweet_count"] = tweet_json["_source"]["retweet_count"]
+		tweet_obj["twitter_id_str"] = tweet_json["_source"]["id_str"]
+		tweet_obj["lang"] = tweet_json["_source"]["lang"]
+		if "place" in tweet_json["_source"]:
+			tweet_obj["place"] = tweet_json["_source"]["place"]
+		if "coordinates" in tweet_json:
+			if "coordinates" in tweet_json["coordinates"]:
+				tweet_obj["coordinates"] = tweet_json["coordinates"]["coordinates"]
+		#print tweet_obj["text"]
+		all_user_tweet_obj.append(tweet_obj)
+	return all_user_tweet_obj
+
 
 def getStoredTweetCount(screen_name):
-    res = es.search(index="depressed_tweets", doc_type="tweepyTweet", body={"query": { "match": {"user.screen_name": screen_name}}})
+    res = es.search(index="depressed_tweets", doc_type="tweepyTweet", body={"query": { "match": {"user.screen_name": screen_name}}}, request_timeout=60)
     #print res
     tweet_count = res['hits']['total']
     return tweet_count
 
 def getMax_Id(screen_name):
-    res = es.search(index="depressed_tweets", doc_type="tweepyTweet", body={"query": { "match": {"user.screen_name": screen_name}}})
+    res = es.search(index="depressed_tweets", doc_type="tweepyTweet", body={"query": { "match": {"user.screen_name": screen_name}}}, request_timeout=60)
     #print("%d documents found" % res['hits']['total'])
     tweet_ids = []
     for doc in res['hits']['hits']:
@@ -50,7 +235,7 @@ def indexTweet(tweet_json):
     print res
 
 def indexUser(index_id, user_profile_json):
-    res = es.index(index = "user_profiles", doc_type = "user_profile", body = user_profile_json, id = index_id)
+    res = es.index(index = "user_profiles", doc_type = "Self_Reported_Profiles_40k", body = user_profile_json, id = index_id)
     print res
 
 #
